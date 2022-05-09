87065edd96ef766d6569e0b2b58b9d74894bf4de
sj <sakurei22@gmail.com>
2022-05-09 11:20:47 +0900

    hook useEffect Deps

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import { useState, useEffect } from "react";
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
   console.log("i run all the time");
 
-  //한번만 실행됨!
+  //[] 한번만 실행됨!
   useEffect(() => {
-    console.log("call the api..");
+    console.log("i run only once");
   }, []);
+  //[keyword]를 바라봄
+  useEffect(() => {
+    console.log("i run when 'keyword' changes.");
+  }, [keyword]);
+  //[counter]를 바라봄
+  useEffect(() => {
+    console.log("i run when 'counter' changes.");
+  }, [counter]);
+  //[counter, keyword]를 바라봄
+  useEffect(() => {
+    console.log("i run when 'counter&keyword' changes.");
+  }, [counter, keyword]);
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
